b4f643ff8f0458fed48652b8e2634eb504ca9730
abrahan1194 <abrahan.1194@gmail.com>
2026-04-21 17:58:33 -0500

    encarpetado general mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ShiftManagement.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
